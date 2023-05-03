29018bbe2ed2f02d09bf2b0916cb14f44fc9eb5d
ttthan1995 <100218581+ttthan1995@users.noreply.github.com>
2023-05-03 23:59:16 +0700

    by Than 20230503

---
+++ eVoucher.Infrastructure/Maps/CampaignUser/CampaignUserMap.cs
@@ -0,0 +1,25 @@
+﻿using eVoucher.Domain.Models;
+using eVoucherApi.Domain.Models;
+
+namespace eVoucher.Infrastructure.Maps
+{
+    public class CampaignUserMap : EntityMap<CampaignUser>
+    {
+        public CampaignUserMap()
+        {
+            Table("CampaignUser");
+
+            Map(p => p.JoinDate);
+           
+
+            HasMany<Campaign>(p => p.Campaign_ID)
+                   .Table("Campaign")
+                   .KeyColumn("Campaign_ID")
+                   .Cascade.AllDeleteOrphan();
+            HasMany<User>(p => p.User_ID)
+                   .Table("User")
+                   .KeyColumn("User_ID")
+                   .Cascade.AllDeleteOrphan();
+        }
+    }
+}
---
+++ eVoucherApi/Models/CampaignUser.cs
@@ -0,0 +1,10 @@
+﻿namespace eVoucherApi.Models
+{
+    public class CampaignUser 
+    {
+        public Guid ID { get; set; }
+        public DateTime JoinDate { get; set; }
+        public Guid User_ID { get; set; }
+        public Guid Campaign_ID { get; set; }
+    }
+}
