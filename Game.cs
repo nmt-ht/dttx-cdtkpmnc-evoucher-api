a6608bc384deda1fb4eb2f23440e1b1931c91259
ttthan1995 <100218581+ttthan1995@users.noreply.github.com>
2023-05-03 19:46:26 +0700

    Add files via upload
    By Than 20230503

---
+++ Game.cs
@@ -0,0 +1,14 @@
+﻿using eVoucher.Domain.SeekWork;
+namespace eVoucherApi.domain.Models
+{
+    public class Game : Entity
+    {
+        public virtual string? Name { get; set; }
+        public virtual string? Description { get; set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+        public virtual string? CreatedBy { get; set; }
+        public virtual string? ModifiedBy { get; set; }
+        public virtual bool IsDeleted { get; set; }
+    }
+}
