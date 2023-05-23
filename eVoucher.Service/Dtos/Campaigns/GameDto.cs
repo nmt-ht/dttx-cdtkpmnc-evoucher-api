50cf38a7bf83ea9a8ec633c7d4989091091c4bf6
Ti Ni <nguyenthithanhthu.tini@gmail.com>
2023-05-24 01:30:12 +0700

    Thư-Add/edit/delete

--- eVoucher.Service/Dtos/Campaigns/GameDto.cs
+++ eVoucher.Service/Dtos/Campaigns/GameDto.cs
@@ -1,11 +1,13 @@
 ﻿namespace eVoucher.Service.Dtos
 {
-    public class GameDto
+    public class CampaignDto
     {
         public Guid Id { get; set; }
         public string Name { get; set; } 
         public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime StartedDate { get; set; }
+        public DateTime ExpiredDate { get; set; }
         public DateTime ModifiedDate { get; set; }
         public Guid ModifiedBy { get; set; }
         public bool IsDeleted { get; set; }
