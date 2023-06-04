6d03a3a24b3a7335cd4097173f9d242cdccfb933
ttthan1995 <100218581+ttthan1995@users.noreply.github.com>
2023-06-04 16:25:10 +0700

    Insert data Campaign - Game - CampaignGame

--- Databases/Others/InsertDataCampaign.sql
+++ Databases/Others/InsertDataCampaign.sql
@@ -1,4 +1,6 @@
-﻿declare @User UNIQUEIDENTIFIER = (select top 1 ID from [user] WHERE UserName = 'Admin')
+﻿-- Campaign
+declare @User UNIQUEIDENTIFIER = (select top 1 ID from [user] WHERE UserName = 'Admin')
+select *  from Campaign where CreatedBy<>@User
 
 insert into Campaign (ID, Name, Description,CreatedDate,StartedDate,ExpiredDate,ModifiedDate, CreatedBy,ModifiedBy,IsDeleted)
 values (NEWID(),N'Freeship 20.000',N'Ăn uống thả ga, FreeShip tới 20.000', GETDATE(),GETDATE(),GETDATE(),GETDATE(),@User,@User,0),
@@ -12,5 +14,55 @@ values (NEWID(),N'Freeship 20.000',N'Ăn uống thả ga, FreeShip tới 20.000'
 (NEWID(),N'Cuoi tuan no ne chot deal 0d',N'Quan moi len san, mon ngon deal 0d ngap tran', GETDATE(),GETDATE(),GETDATE(),GETDATE(),@User,@User,0),
 (NEWID(),N'FreeShip tới 35.000đ',N'FreeShip tới 35.000đ', GETDATE(),GETDATE(),GETDATE(),GETDATE(),@User,@User,0)
 
+-- Game
+declare @User UNIQUEIDENTIFIER = (select top 1 ID from [user] WHERE UserName = 'Admin')
+insert into Game(ID,Name,Description,CreatedDate,ModifiedDate,CreatedBy,ModifiedBy,IsDeleted)
+values (NEWID(),'Teris','Teris',GETDATE(),GETDATE(),@User,@User,0),
+		(NEWID(),'TicTacToe','Tic Tac Toe',GETDATE(),GETDATE(),@User,@User,0),
+		(NEWID(),'ConnectFour','ConnectFour',GETDATE(),GETDATE(),@User,@User,0)
 
-select *  from Campaign
+-- CampaignGame
+declare @Game1 UNIQUEIDENTIFIER = (select top 1 ID from Game WHERE Name = 'Teris')
+declare @Game2 UNIQUEIDENTIFIER = (select top 1 ID from Game WHERE Name = 'TicTacToe')
+declare @Game3 UNIQUEIDENTIFIER = (select top 1 ID from Game WHERE Name = 'ConnectFour')
+
+insert into CampaignGame(ID,Campaign_ID_FK,Game_ID_FK)
+values (NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Freeship 20.000'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Freeship 20.000'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Freeship 20.000'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Siêu deal 50%'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Siêu deal 50%'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Siêu deal 50%'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 99.000đ'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 99.000đ'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 99.000đ'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm dến 40.000đ'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm dến 40.000đ'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm dến 40.000đ'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Bão món deal 0d'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Bão món deal 0d'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Bão món deal 0d'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm tới 166.000đ'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm tới 166.000đ'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm tới 166.000đ'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 50.000đ'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 50.000đ'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 50.000đ'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 50%'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 50%'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Giảm 50%'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Cuoi tuan no ne chot deal 0d'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Cuoi tuan no ne chot deal 0d'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'Cuoi tuan no ne chot deal 0d'),@Game3),
+
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'FreeShip tới 35.000đ'),@Game1),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'FreeShip tới 35.000đ'),@Game2),
+		(NEWID(), (SELECT ID FROM Campaign WHERE Name=N'FreeShip tới 35.000đ'),@Game3)
