42a9e8d2b1e818607dcb1ecb52d86e379460861e
ttthan1995 <100218581+ttthan1995@users.noreply.github.com>
2023-06-17 21:56:06 +0700

    spr_GetAllCampaign

---
+++ Databases/StoreProcedures/spr_GetAllCampaign.sql
@@ -0,0 +1,25 @@
+CREATE PROC  [dbo].[spr_GetAllCampaign] 
+(
+	@CreatedDateFrom smalldatetime=NULL,
+	@CreatedDateTo smalldatetime=NULL,
+	@PartnerID UNIQUEIDENTIFIER=NULL
+
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	
+	SELECT c.ID, c.Name, c.[Description], c.CreatedDate, c.StartedDate, c.ExpiredDate,
+		   c.ModifiedDate, c.CreatedBy, c.ModifiedBy, c.IsDeleted
+	FROM Campaign c
+	LEFT JOIN PartnerCampaign pc ON pc.Campaign_ID_FK = c.ID
+	WHERE c.CreatedDate >= @CreatedDateFrom AND c.CreatedDate <=@CreatedDateTo AND pc.Partner_ID_FK=@PartnerID
+	
+	SET NOCOUNT OFF
+END
+
+
+
+
+
