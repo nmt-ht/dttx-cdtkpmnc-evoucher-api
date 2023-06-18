5d11d6cd7fd65b87589065fa64c0988c1841d72d
ttthan1995 <100218581+ttthan1995@users.noreply.github.com>
2023-06-18 12:32:52 +0700

    spr_GetAllCampaign

--- Databases/StoreProcedures/spr_GetAllCampaign.sql
+++ Databases/StoreProcedures/spr_GetAllCampaign.sql
@@ -11,7 +11,7 @@ BEGIN
 	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 	
 	SELECT c.ID, c.Name, c.[Description], c.CreatedDate, c.StartedDate, c.ExpiredDate,
-		   c.ModifiedDate, c.CreatedBy, c.ModifiedBy, c.IsDeleted
+		   c.ModifiedDate, c.CreatedBy, c.ModifiedBy, c.IsDeleted,c.Image,pc.Partner_ID_FK
 	FROM Campaign c
 	LEFT JOIN PartnerCampaign pc ON pc.Campaign_ID_FK = c.ID
 	WHERE c.CreatedDate >= @CreatedDateFrom AND c.CreatedDate <=@CreatedDateTo AND pc.Partner_ID_FK=@PartnerID
